12d71f30b8af263b1a11ecc354737ae1eeb4d241
Ben Podrazhansky <benpodrazhansky@gmail.com>
2022-04-09 18:40:47 -0400

    Fixed spelling error in file name

--- Data/__init__.py
+++ Data/__init__.py
@@ -11,4 +11,4 @@ TIME_STEP = timedelta(seconds=1)
 
 
 from Data.setupDB import NormalDBPath, AttackDBPath
-from Data.dbQuerry import getAttackDataIterator, getNormalDataIterator
+from Data.dbQuery import getAttackDataIterator, getNormalDataIterator
